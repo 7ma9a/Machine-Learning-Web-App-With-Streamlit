ca0fdba481777ffc5184e12fe8d3e5c6e8760db6
Muhammad Zidane <56770204+7ma9a@users.noreply.github.com>
2023-02-14 14:11:35 +0200

    Update mian.py

--- mian.py
+++ mian.py
@@ -5,5 +5,6 @@ st.write("""
 # Explore different classifier
 Which one is the best?
 """)
-dataset_names = st.selectbox("Select dataset",("Iris","Breast Cancer","ًWine Dataset"))
-st.write(dataset_names)
+dataset_names = st.sidebar.selectbox("Select dataset",("Iris","Breast Cancer","ًWine Dataset"))
+
+classifier_name = st.sidebar.selectbox("Select Classifier",("KNN","SVM","Random Forest"))
